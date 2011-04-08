59831b1dee5aa0f63b4207b6b8f1b2562849a9ae
Michael Whapples <mwhapples@aim.com>
2011-04-08 12:36:24 +0100

    Changed license to apache 2.0.

--- src/main/java/org/mwhapples/jlouis/TranslationException.java
+++ src/main/java/org/mwhapples/jlouis/TranslationException.java
@@ -1,8 +1,21 @@
 package org.mwhapples.jlouis;
 /*
- * Copyright (c) 2010 Michael Whapples
- * 
- * This code is released under the QPL which can be seen in the file LICENSE distributed with this source code.
+ * Copyright (c) 2010-2011 Michael whapples
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For attribution notices please see the file NOTICES.TXT which should be
+ * included in the distribution.
  */
 
 public class TranslationException extends Exception {
